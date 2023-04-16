e2a694a7e2ba0c24c7f741d66e533bfdb6f760bf
Abdur Rahman <arwithlpd>
2023-04-17 00:34:30 +0600

    collection

---
+++ Advanced/CollectionsCreate/Collections/src/Main.java
@@ -0,0 +1,47 @@
+// java Collection example:
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Create a collection
+        Collection<String> collection = new ArrayList<String>();
+        collection.add("New York");
+        collection.add("Atlanta");
+        collection.add("Dallas");
+        collection.add("Madison");
+
+        System.out.println("A list of cities in collection:");
+        System.out.println(collection);
+
+        System.out.println("Is Dallas in collection? " + collection.contains("Dallas"));
+
+        collection.remove("Dallas");
+        System.out.println("Dallas is removed from collection");
+
+        System.out.println("Is Dallas in collection? " + collection.contains("Dallas"));
+
+        Collection<String> collection2 = new ArrayList<String>();
+        collection2.add("Seattle");
+        collection2.add("Portland");
+        collection2.add("Los Angeles");
+        collection2.add("Atlanta");
+
+        System.out.println("A list of cities in collection2:");
+        System.out.println(collection2);
+
+        System.out.println("Is collection2 a subset of collection? " + collection.containsAll(collection2));
+
+        collection.addAll(collection2);
+        System.out.println("A list of cities in collection:");
+        System.out.println(collection);
+
+        collection.removeAll(collection2);
+        System.out.println("A list of cities in collection:");
+        System.out.println(collection);
+
+        collection.retainAll(collection2);
+        System.out.println("A list of cities in collection:");
+        System.out.println(collection);
+    }
+}
